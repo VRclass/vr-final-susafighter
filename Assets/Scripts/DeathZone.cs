5329450e758e6817afd401134205debab7980e25
susafighter <47373483+susafighter@users.noreply.github.com>
2019-04-18 12:06:49 -0500

    New Portals
    added different portals and new city portal

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour {
+    public Transform spawnPoint;
+
+    private void OnTriggerEnter(Collider other) {
+        other.gameObject.transform.position = spawnPoint.position;
+    }
+}
